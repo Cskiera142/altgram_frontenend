2a74f82628c19f3518c5a28f2ad5778154f0ea6e
Colton Skiera <cskiera142@gmail.com>
2024-03-24 18:42:45 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+
+import "./globals.css";
+import SignInForm from "./_auth/forms/SignInForm";
+import { Home } from "./_root/pages";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+
+function App() {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function SignupForm() {
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
+
+export default SignupForm;
